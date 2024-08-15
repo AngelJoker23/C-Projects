b9db624c2e93978472714cc4f0c506cfeb85eeee
AngelJoker23 <127627388+AngelJoker23@users.noreply.github.com>
2024-08-15 03:11:06 -0500

    Add files via upload

---
+++ C++ Projects/Number Guessing Game/numberguessinggame.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+int main()
+{
+	srand(static_cast<unsigned int>(time(0)));
+
+	int secretNumber = rand() % 100 + 1;
+	int guess = 0;
+
+	std::cout << "I have chosen a number between 1 and 100." << std::endl;
+	std::cout << "Can you guess what it is?" << std::endl;
+
+	while (guess != secretNumber)
+	{
+		std::cout << "Enter your guess: ";
+		std::cin >> guess;
+
+		if (guess > secretNumber)
+		{
+			std::cout << "Too high! Try again!" << std::endl;
+		}
+		else if (guess < secretNumber)
+		{
+			std::cout << "Too low! Try again!" << std::endl;
+		}
+		else
+		{
+			std::cout << "Congratulations! You guessed my number!" << std::endl;
+		}
+	}
+
+	return 0;
+}
